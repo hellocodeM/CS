4f91511410324e6f2513d4979f2717acaaeedfd2
HelloCodeMing <huanmingwong@163.com>
2015-04-27 11:57:29 +0800

    designing stack model

--- src/opcode.hpp
+++ src/opcode.hpp
@@ -1,10 +1,73 @@
 
-#define OPCODER_H
+#define OPCODE_H
 
 namespace CS {
-class Opcode {
+
+class Instruction {
     public:
 
     private:
+};
+
+class StackModel {
+    public:
+        StackModel() {}
+
+        StackModel(const vector<Instruction>& instructions) {
+
+        }
+
+        OpCode Dump() const {
+
+        }
+
+        void Push(int i) {
+
+        }
+
+        void Pop() {
+
+        }
+
+        void Top() {
+
+        }
+        
+
+        void Mov(int dst, int src) {
+        }
+
+        void Assign(int dst, int val) {
+
+        }
+
+        void Call() {
+
+        }
+
+        /* numeric operation */
+        void Add() {
+
+        }
+        
+        void Sub() {
+
+        }
+
+        void Mul() {
+
+        }
+
+        void Div() {
+
+        }
+
+        void Mod() {
+
+        }
+        
+
+    private:
+        vector<Instruction>
 };
 }
